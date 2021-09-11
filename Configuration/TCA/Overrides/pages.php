d466917f90901bbc71ea2cf609a9e2165cbcd777
lina.wolf <lwolf@w-commerce.de>
2021-09-11 23:05:53 +0200

    [TASK] Prepare Code Blocks to be included in the template Break up TypoScript in Separate files This facilitates Automatic Code-Blocks

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,11 +1,17 @@
 <?php
 defined('TYPO3') || die();
 
-/**
- * Default PageTS for site_package
- */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
-    'site_package',
-    'Configuration/TsConfig/Page/Page.tsconfig',
-    'TYPO3 Documentation Team Site Package Example'
-);
+call_user_func(function () {
+    /**
+     * Extension key
+     */
+    $extensionKey = 'site_package';
+    /**
+     * Default PageTS for site_package
+     */
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+        $extensionKey,
+        'Configuration/TsConfig/Page/Page.tsconfig',
+        'TYPO3 Documentation Team Site Package Example'
+    );
+});
