39b91e5463f8c54840829cf49f1f86dedc186d21
lina.wolf <lwolf@w-commerce.de>
2021-09-19 22:11:32 +0200

    [TASK] Rewrite TypoScript and Fluid Chapters
    Static Templates as example of the process of turning a static file into a Fluid Template. Move some TypoScript.

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,19 +1,5 @@
 <?php
 
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code of
- * the TYPO3 source package.
- *
- * The TYPO3 project - inspiring people to share!
- */
-
 $EM_CONF[$_EXTKEY] = [
     'title' => 'TYPO3 Site Package',
     'description' => 'TYPO3 Site Package',
