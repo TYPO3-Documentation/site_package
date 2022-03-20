a05f617343779e3f6f3f39e5c15391e4b5e5cd1e
lina.wolf <lwolf@w-commerce.de>
2022-03-20 10:07:00 +0100

    [FEATURE] EXT:site_package/Configuration/page.tsconfig' is loaded automatically

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,6 +4,3 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3') or die();
 
-ExtensionManagementUtility::addPageTSConfig("
-    @import 'EXT:site_package/Configuration/page.tsconfig'
-");
