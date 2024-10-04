bcebfb379c97d995e256653026049ca886d37a20
lina.wolf <lwolf@w-commerce.de>
2024-10-04 17:54:43 +0200

    [TASK] Switch to bootstrap 5.3
    Releases: main

---
+++ Resources/Public/JavaScript/main.js
@@ -0,0 +1,7 @@
+// Select the button by its ID
+const button = document.getElementById('exampleButton');
+
+// Add a click event listener
+button.addEventListener('click', function() {
+  alert('Hello world!');
+});
