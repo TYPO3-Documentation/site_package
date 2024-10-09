7d98bba1c57defd3226fa25a21712f6b7a5f4cc8
lina.wolf <lwolf@w-commerce.de>
2024-10-09 06:34:15 +0200

    [TASK] Delete all files not needed for the minimal site package

--- Resources/Public/JavaScript/main.js
+++
@@ -1,7 +0,0 @@
-// Select the button by its ID
-const button = document.getElementById('exampleButton');
-
-// Add a click event listener
-button.addEventListener('click', function() {
-  alert('Hello world!');
-});
