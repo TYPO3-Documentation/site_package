00b715c47f27f192bd7cf23f5d9dc4711db8facd
Karsten Nowak <nowak@undkonsorten.com>
2026-04-28 08:45:45 +0200

    [TASK] Update TYPO3 requirements to 14.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,8 +7,8 @@ $EM_CONF[$_EXTKEY] = [
     "category" => "templates",
     "constraints" => [
         "depends" => [
-            "typo3" => "14.0.0-14.3.99",
-            "fluid_styled_content" => "14.0.0-14.3.99",
+            "typo3" => "14.3.0-14.3.99",
+            "fluid_styled_content" => "14.3.0-14.3.99",
             "content_blocks" => "2.0.0-2.99.99",
         ],
         "conflicts" => [],
